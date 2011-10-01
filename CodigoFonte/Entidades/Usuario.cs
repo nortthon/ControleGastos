e5ad33b26b3e2a1fc37a450bcee2d3cb8e8f77ae
nortthon <nortthon@fb21520e-695d-48df-927d-5f3092feb67a>
2011-10-01 05:31:26 +0000

    Inclusão do projeto.

---
+++ CodigoFonte/Entidades/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DIAD.ControleDeGastos.Entidades
+{
+    public class Usuario
+    {
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        public String Senha { get; set; }
+    }
+}
